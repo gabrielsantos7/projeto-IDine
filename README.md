9bc9a247d70463636b8a9a68d8fbd54e004ad7e9
gabrielSantos3 <gabrielss432112@gmail.com>
2023-05-21 11:49:28 -0300

    CSS reutilizável adicionado
    Criei o header e o footer que serão utilizados em todas as páginas.

--- README.md
+++ README.md
@@ -1,10 +1,53 @@
-# Projeto-IFood
+# Projeto-IDine🍔🛵
 
-> Quando o projeto tiver pronto, eu faço a descrição mais detalhada. Por enquanto se atentem somente à estruturação
+Projeto final da primeira unidade na disciplina de Web II, ministrada pelo professor [Thiago Miranda](https://github.com/mirandathiago "Github do professor Thiago").  Fizemos um conjunto de páginas que simula uma plataforma de delivery de alimentos, semelhante ao iFood.
 
-# 📂 Organização de pastas e arquivos do projeto iFood
+## Descrição 📝
 
-Este documento descreve a estrutura de pastas e arquivos utilizada no projeto iFood, com o objetivo de facilitar a organização do projeto e sua manutenção. Logo, é muito importante saber sobre a organização para **não sair colocando arquivos no lugar errado.** A seguir está uma explicação sobre cada pasta principa e suas subpastas. Caso tenham alguma dúvida, me procurem antes de fazer qualquer modificação.
+O projeto consiste na construção de um site para empresa do ramo de delivery de alimentos, inspirada no iFood. O objetivo principal do projeto é exibir os produtos utilizando PHP por meio de arrays multidimensionais e associativos, assim como a construção de uma página mais detalhada sobre cada produto.
+
+# Organização de pastas e arquivos do projeto IDine📂
+
+Segue abaixo a estruturação de pastas, com o objetivo de facilitar a organização do projeto e sua manutenção. Logo, é muito importante saber sobre a organização para **não sair colocando arquivos no lugar errado.** A seguir está uma explicação sobre cada pasta principal e suas subpastas. Caso tenham alguma dúvida, me procurem antes de fazer qualquer modificação.
+
+```css
+projeto-idine/
+│
+├── assets/
+│   ├── css/
+│   │   ├── style.css  				⚙️ CSS principal do projeto
+│   │   ├── header-and-footer.css  		⚙️ CSS reutilizável para o cabeçalho e rodapé
+│   │   ├── produtos.css  			⚙️ CSS específico da página de produtos
+│   │   ├── historia.css  			⚙️ CSS específico da página de história
+│   │   ├── cadastro.css  			⚙️ CSS específico da página de cadastro
+│   │   └── page4.css  				⚙️ CSS específico da página 4
+│   ├── js/
+│   │   ├── script.js  				⚙️ JavaScript principal do projeto
+│   │   └── produtos.js   			⚙️ JavaScript específico da página de produtos
+│   └── img/
+│       ├── icons/   				🖼️ Pasta para ícones e logos
+│       ├── products/     			🖼️ Pasta para imagens dos produtos
+│       └── components/   			🖼️ Pasta para os componentes estáticos do site
+│
+├── pages/
+│   ├── index.php     				📄 Página principal do projeto
+│   ├── produtos.php     			📄 Página de exibição dos produtos
+│   ├── historia.php     			📄 Página sobre a história da empresa
+│   ├── cadastro.php     			📄 Página de cadastro
+│   └── produto.php     			📄 Página de exibição individual de um produto
+│
+├── services/
+│   └── data/
+│       ├── produtos.php  			🗃️ Array associativo de produtos
+│       ├── comidas.php 			🗃️ Array associativo de comidas
+│       ├── supermercados.php  			🗃️ Array associativo de supermercados
+│       └── restaurantes.php  			🗃️ Array associativo de restaurantes
+│
+├── util/
+│   └── utils.php     				🔧 Funções utilitárias do projeto
+│
+└── README.md         				📖 Arquivo de documentação do projeto
+```
 
 ## 📁 assets
 
@@ -22,18 +65,23 @@ Arquivos JavaScript utilizados pelo projeto, divididos por página, e um arquivo
 
 Imagens utilizadas pelo projeto, divididas por tipo: ícones e logos na pasta `icons/`, imagens de produtos na pasta `products/`, e outros componentes na pasta `components/`.
 
-### 📁 fonts
+## pages📄
 
-Fontes utilizadas pelo projeto, caso necessário.
+A pasta `pages` contém as páginas do projeto, separadas em arquivos PHP ou HTML.
 
-## 📁 pages
-
-A pasta `pages` contém as páginas do projeto, separadas em arquivos PHP ou HTML. 
-
-## 📁 util
+## util🛠️
 
 A pasta `util` contém funções utilitárias do projeto, se necessário.
 
-## 📄 README.md
+## Como Executar o Projeto ▶️
 
-É este arquivo que você está lendo, que contém informações sobre a organização do projeto.
+Para executar o projeto iDine, siga as instruções abaixo:
+
+1. Faça o download dos arquivos do projeto para o seu computador.
+2. Configure um ambiente de desenvolvimento PHP, como o XAMPP ou WAMP, para executar o projeto localmente.
+3. Mova os arquivos do projeto para o diretório apropriado do servidor web local.
+4. Inicie o servidor web local e acesse o projeto através do seu navegador, utilizando o endereço local correspondente.
+
+## Licença 📜
+
+Este projeto está licenciado sob a [Licença MIT](https://opensource.org/licenses/MIT). Consulte o link para obter mais informações.
---
+++ pages/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IDine-Home</title>
+    <!-- 
+        ? Como combinado, utilizaremos dois arquivos CSS diferentes:
+        * O primeiro deve ser o CSS da sua própria página (por exemplo, para a página produtos, tem o produtos.css)
+        * O segundo é o CSS do header e do footer, que é reutilizável em todo o projeto (todas as páginas terão o mesmo cabeçalho e rodapé, portanto, não há  anecessidade de criar cada um deles em cada arquivo CSS).
+    -->
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/header-and-footer.css">
+    <!-- Este link é para conseguir os ícones com a tag i (ícone do instagram, whatsapp, etc.) -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<!-- 
+    Todo o conteúdo, dividido em articles, será colocado dentro da tag main, que representa o bloco do conteúdo principal da página. O main fica entre o header e o footer. 
+-->
+<body>
+    <header id="topo">
+        <h1 id="h1-sumir">IDine - Serviço de entrega</h1>
+            <a href="index.html">
+                <img src="../assets/img/icons/logo-branca.png" alt="Logo da IDine" id="img-logo">
+            </a>
+            <nav id="menu">
+                <ul class="nav-list">
+                    <li><a href="" class="link-nav">Comidas e Produtos</a></li>
+                    <li><a href="" class="link-nav">Restaurantes e Supermercados</a></li>
+                    <li><a href="" class="link-nav">Sobre Nós</a></li>
+                </ul>
+            </nav>
+
+            <a href="cadastro.html">
+                <div class="btn-branco">Cadastrar</div>
+            </a>
+    </header>
+
+    <main>
+        <!-- Coloque o seu conteúdo aqui -->
+    </main>
+
+    <footer>
+        <div id="footer_content">
+            <div id="footer_contacts">
+                <figure>
+                    <img src="../assets/img/icons/logo-branca.png" alt="Logo da IDine" id="footer-logo">
+                    <figcaption>Sabor e praticidade <br>na palma da mão.</figcaption>
+                </figure>
+                <div id="footer_social_media">
+                    <a href="#" class="footer-link" id="whatsapp">
+                        <i class="fa-brands fa-whatsapp"></i>
+                    </a>
+
+                    <a href="#" class="footer-link" id="instagram">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+
+                    <a href="#" class="footer-link" id="facebook">
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+
+                    <a href="#" class="footer-link" id="twitter">
+                        <i class="fa-brands fa-twitter"></i>
+                    </a>
+                </div>
+            </div>
+            
+            <div class="footer-list">
+                <h3>IDine</h3>
+                <address>
+                    Travessa dos Negócios,  nº 311 <br>
+                    Claudinópolis/BR <br> CEP 06.450-902
+                </address>
+            </div>
+
+            <ul class="footer-list">
+                <li>
+                    <h3>Parceiros</h3>
+                </li>
+                <li>
+                    <a href="https://www.nobelpack.com.br/" target="_blank" class="footer-link">NobelPack</a>
+                </li>
+                <li>
+                    <a href="https://www.ambev.com.br/" target="_blank" class="footer-link" >Ambev</a>
+                </li>
+                <li>
+                    <a href="https://indrive.com/pt/city/" target="_blank" class="footer-link">InDrive</a>
+                </li>
+            </ul>
+
+            <div id="footer_subscribe">
+                <h3>Se inscreva</h3>
+
+                <p>
+                    Digite seu e-mail para receber atualizações diárias sobre promoções, eventos, etc.
+                </p>
+
+                <div id="input_group">
+                    <input type="email" id="email" placeholder="seu e-mail...">
+                    <button>
+                        <i class="fa-regular fa-envelope"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div id="footer_copyright">
+            &#169 2023 todos os direitos reservados
+        </div>
+    </footer>
+</body>
+</html>
