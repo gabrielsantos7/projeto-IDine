e38cbe80b2d9c572614c9378ca611540f8ae2e35
Cláudio Augusto <clmarques.dev@gmail.com>
2023-05-21 13:24:56 -0300

    Criando array $restaurantes
    Apenas exemplos iniciais.

---
+++ services/data/restaurantes.php
@@ -0,0 +1,44 @@
+<?php
+
+$restaurantes = [   
+    [        
+        "nome" => "Delícias do Sabor",
+        "endereco" => "Gerente de Vendas",
+        "image" => "#",
+        "telefone" => 9999-9999,        
+        "horarios" => "Segunda a sábado, das 11h às 23h",
+        "especialização" => "Culinária contemporânea com toques regionais"
+    ],
+    [        
+        "nome" => "Gastrô Fusion",
+        "endereco" => "Avenida dos Sabores, 456, Centro Gourmet",
+        "image" => "#",
+        "telefone" => 9999-9998,        
+        "horarios" => "Terça a domingo, das 18h às 00h",
+        "especialização" => "Fusão de sabores internacionais em pratos sofisticados"
+    ],
+    [        
+        "nome" => "Cantinho Gourmet",
+        "endereco" => "Praça dos Aromas, 789, Vila Gastronômica",
+        "image" => "#",
+        "telefone" => 9999-9997,        
+        "horarios" => "Quarta a domingo, das 12h às 15h e das 19h às 23h",
+        "especialização" => "Culinária gourmet com ingredientes frescos e sazonais"
+    ],
+    [        
+        "nome" => "",
+        "endereco" => "",
+        "image" => "#",
+        "telefone" => 9999-9996,        
+        "horarios" => "",
+        "especialização" => ""
+    ],
+    [        
+        "nome" => "",
+        "endereco" => "",
+        "image" => "#",
+        "telefone" => 9999-9995,        
+        "horarios" => "",
+        "especialização" => "Culinária contemporânea com toques regionais"
+    ]
+];
