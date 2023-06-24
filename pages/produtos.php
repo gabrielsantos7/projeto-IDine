378c20eb9cd134b9a37f587f67ac6f45cdc4e1f6
Gabriel Silva Santos <91688865+gabrielsantos7@users.noreply.github.com>
2023-06-24 17:00:09 -0300

    titles corrigidos

--- pages/produtos.php
+++ pages/produtos.php
@@ -11,20 +11,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IDine-Home</title>
-    <!-- 
-        ? Como combinado, utilizaremos dois arquivos CSS diferentes:
-        * O primeiro deve ser o CSS da sua própria página (por exemplo, para a página produtos, tem o produtos.css)
-        * O segundo é o CSS do header e do footer, que é reutilizável em todo o projeto (todas as páginas terão o mesmo cabeçalho e rodapé, portanto, não há  anecessidade de criar cada um deles em cada arquivo CSS).
-    -->
+    <title>Produtos</title>
+
     <link rel="stylesheet" href="../assets/css/header-and-footer.css">
     <link rel="stylesheet" href="../assets/css/produtos.css">
-    <!-- Este link é para conseguir os ícones com a tag i (ícone do instagram, whatsapp, etc.) -->
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<!-- 
-    Todo o conteúdo, dividido em articles, será colocado dentro da tag main, que representa o bloco do conteúdo principal da página. O main fica entre o header e o footer. 
--->
+
 <body>
     <header id="topo">
         <h1 id="h1-sumir">IDine - Serviço de entrega</h1>
@@ -46,7 +40,6 @@
     </header>
 
     <main>
-        <!-- Coloque o seu conteúdo aqui -->
         <section id="section-comentario" class="section">
             <article id="group-section">
                 <h2 id="text-comentario">Comentários</h2>
